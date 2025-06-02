08162f8c3d56ecd5b874ad4391fe85cbdccc799f
gymanagement <firstproject323@gmail.com>
2025-06-02 18:05:58 +0300

    Delete gym_management directory

--- gym_management/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import sub_management
-from . import gym_member
-from . import gym_members
-from . import membership
-from . import workout_plans
--- gym_management/models/membership.py
+++
@@ -1,8 +0,0 @@
-from odoo import models,fields
-
-class Membership(models.Model):
-    _name = 'membership.membership'
-
-    member_id = fields.Many2one('res.partner', string="Member")
-    trainer_id = fields.Many2one('hr.employee', string="Trainer")
-    subscription_plan= fields.Char(String="Subscription Plan")
